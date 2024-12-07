dc03a38dc8fff1ba251842a3315ee439a4d74580
Rusiru Salwathura <rusirusalwathura@icloud.com>
2024-12-08 01:47:09 +0530

    added paths for chatbot and imageupload

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import LandingPage from './pages/LandingPage';
 import Login from './pages/Login'; 
 import Register from './pages/Register';
 import UserDashboard from './pages/UserDashboard';
+import Chatbot from './components/Chatbot';
+import ImageUpload from './components/ImageUpload';
 
 function App() {
   console.log("App rendered");
@@ -14,6 +16,8 @@ function App() {
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/userdashboard' element={<UserDashboard />} />
+        <Route path='/chatbot' element={<Chatbot />} />
+        <Route path='/imageupload' element={<ImageUpload />} />
       </Routes>
     </Router>
   );
